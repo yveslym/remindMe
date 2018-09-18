07533f0503030ad22ee8619f044bf6f5ec686f0a
yves songolo <yves.songolo@gmail.com>
2018-09-18 16:35:51 -0700

    add user and group model

---
+++ remindMe/Models/Group.swift
@@ -0,0 +1,21 @@
+//
+//  Group.swift
+//  remindMe
+//
+//  Created by Yves Songolo on 9/14/18.
+//  Copyright © 2018 Yves Songolo. All rights reserved.
+//
+
+import Foundation
+struct Group: Codable{
+    var id: String
+    var name: String
+    var longitude: Double
+    var latitude: Double
+    
+    func toDictionary() ->[String: Any]{
+        let data = try! JSONEncoder().encode(self)
+        let json = try! JSONSerialization.jsonObject(with: data, options: [])
+        return json as! [String : Any]
+    }
+}
---
+++ remindMe/Models/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  remindMe
+//
+//  Created by Yves Songolo on 9/14/18.
+//  Copyright © 2018 Yves Songolo. All rights reserved.
+//
+
+import Foundation
+struct User: Codable{
+    var name: String
+    var id: String
+    var email: String
+    
+    func toDictionary() ->[String: Any]{
+        let data = try! JSONEncoder().encode(self)
+        let json = try! JSONSerialization.jsonObject(with: data, options: [])
+        return json as! [String : Any]
+    }
+}
