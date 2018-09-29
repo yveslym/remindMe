cdb9e94233147a0ce1071fd624e9c0a4b395d3d7
MediBoss <mediassumani49@gmail.com>
2018-09-29 15:28:05 -0700

    initial commit

---
+++ remindMe/Controllers/SignInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignInViewController.swift
+//  remindMe
+//
+//  Created by Medi Assumani on 9/29/18.
+//  Copyright © 2018 Yves Songolo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FBSDKCoreKit
+import GoogleSignIn
+import FBSDKLoginKit
+
+class SignInViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ remindMe/Controllers/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  remindMe
+//
+//  Created by Medi Assumani on 9/29/18.
+//  Copyright © 2018 Yves Songolo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleSignIn
+import FBSDKLoginKit
+
+class SignUpViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
